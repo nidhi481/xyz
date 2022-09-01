daacb09cf760142b0ef939b38edfe5209faef1b8
AmanRanjanSingh <aman.tutree@gmail.com>
2022-09-01 16:24:35 +0530

    Commit first

--- src/test/java/syndupdemo/synkdupdemo/practice.java
+++ src/test/java/syndupdemo/synkdupdemo/practice.java
@@ -3,7 +3,7 @@ package syndupdemo.synkdupdemo;
 public class practice {
 	public static void main(String[] args) {
 
-		System.out.println("hello");
+		System.out.println("hello Nidhi Mam");
 
 	}
 }
